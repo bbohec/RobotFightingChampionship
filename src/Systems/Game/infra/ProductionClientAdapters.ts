9c5824079af1a314c6838d7e2e24abcc85230b5c
Benjamin Bohec <bbohec.pro@gmail.com>
2021-11-05 16:25:53 +0100

    WIP Production Event Bus

--- src/Systems/Game/infra/ProductionClientAdapters.ts
+++ src/Systems/Game/infra/ProductionClientAdapters.ts
@@ -3,18 +3,20 @@ import { EventInteractor } from '../../../EventInteractor/port/EventInteractor'
 import { DrawingPort } from '../../Drawing/port/DrawingPort'
 import { InMemorySystemRepository } from '../../Generic/infra/InMemorySystemInteractor'
 import { FakeIdentifierAdapter } from '../../LifeCycle/infra/FakeIdentifierAdapter'
+import { IdentifierAdapter } from '../../LifeCycle/port/IdentifierAdapter'
 import { InMemoryNotificationAdapter } from '../../Notification/infra/InMemoryNotificationAdapter'
 import { clientAdapters } from '../port/clientAdapters'
 
 export class ProductionClientAdapters implements clientAdapters {
-    constructor (drawingAdapter:DrawingPort, eventInteractor:EventInteractor) {
+    constructor (drawingAdapter:DrawingPort, eventInteractor:EventInteractor, playerId:string) {
         this.eventInteractor = eventInteractor
         this.drawingInteractor = drawingAdapter
+        this.identifierInteractor = new FakeIdentifierAdapter([playerId])
     }
 
+    identifierInteractor: IdentifierAdapter
     eventInteractor: EventInteractor
     drawingInteractor:DrawingPort
-    identifierInteractor = new FakeIdentifierAdapter()
     notificationInteractor= new InMemoryNotificationAdapter()
     systemInteractor = new InMemorySystemRepository();
     entityInteractor = new InMemoryEntityRepository();
--- src/public/scripts/game.ts
+++ src/public/scripts/game.ts
@@ -1,4 +1,3 @@
-import { InMemoryEventBus } from '../../Event/infra/InMemoryEventBus'
 import { WebClientEventInteractor } from '../../EventInteractor/infra/WebClientEventInteractor'
 import { serverFullyQualifiedDomainName } from '../../EventInteractor/port/testUtilities'
 import { PixijsDrawingAdapter } from '../../Systems/Drawing/infra/PixijsDrawingAdapter'
@@ -6,16 +5,16 @@ import { v1 as uuid } from 'uuid'
 import { ProductionClientAdapters } from '../../Systems/Game/infra/ProductionClientAdapters'
 import { ClientGameSystem } from '../../Systems/Game/ClientGame'
 import { defaultHTTPWebServerPort } from '../../EventInteractor/infra/WebServerEventInteractor'
+import { ProductionEventBus } from '../../Event/infra/ProductionEventBus'
 const loadClient = (playerId:string) => {
-    const productionClientEventBus = new InMemoryEventBus()
+    const productionClientEventBus = new ProductionEventBus()
     const productionClientDrawingAdapter = new PixijsDrawingAdapter(productionClientEventBus)
     const productionClientEventInteractor = new WebClientEventInteractor(serverFullyQualifiedDomainName, defaultHTTPWebServerPort, playerId, productionClientEventBus)
-    const productionClientAdapters = new ProductionClientAdapters(productionClientDrawingAdapter, productionClientEventInteractor)
     const resizePixiCanvas = () => productionClientDrawingAdapter.changeResolution({ x: window.innerWidth, y: window.innerHeight })
     window.addEventListener('resize', resizePixiCanvas)
     productionClientDrawingAdapter.addingViewToDom(document.body)
     resizePixiCanvas()
     productionClientEventInteractor.start()
-    return new ClientGameSystem(productionClientAdapters)
+    return new ClientGameSystem(new ProductionClientAdapters(productionClientDrawingAdapter, productionClientEventInteractor, playerId))
 }
 loadClient(uuid())
