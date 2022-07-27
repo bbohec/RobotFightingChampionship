ff9ea3d247a6057cc18cc081f5c87da7e2c265c8
Benjamin Bohec <bbohec.pro@gmail.com>
2022-07-27 14:00:02 +0200

    hexagonal architecture event interactor

--- src/Systems/Game/infra/FakeClientAdapters.ts
+++ src/Systems/Game/infra/FakeClientAdapters.ts
@@ -5,10 +5,10 @@ import { FakeIdentifierAdapter } from '../../LifeCycle/infra/FakeIdentifierAdapt
 import { IdentifierAdapter } from '../../LifeCycle/port/IdentifierAdapter'
 import { clientAdapters } from '../port/clientAdapters'
 import { InMemoryNotificationAdapter } from '../../Notification/infra/InMemoryNotificationAdapter'
-import { InMemoryClientEventInteractor } from '../../../EventInteractor/infra/client/InMemoryClientEventInteractor'
-import { InMemoryEventBus } from '../../../infra/event/InMemoryEventBus'
+import { InMemoryClientEventInteractor } from '../../../infra/eventInteractor/client/InMemoryClientEventInteractor'
+import { InMemoryEventBus } from '../../../infra/eventBus/InMemoryEventBus'
 import { InMemoryControllerAdapter } from '../../Controller/infra/InMemoryControllerAdapter'
-import { InMemoryServerEventInteractor } from '../../../EventInteractor/infra/server/InMemoryServerEventInteractor'
+import { InMemoryServerEventInteractor } from '../../../infra/eventInteractor/server/InMemoryServerEventInteractor'
 
 export class FakeClientAdapters implements clientAdapters {
     constructor (clientId:string, nextIdentifiers?:string[]) {
--- src/public/scripts/test.ts
+++ src/public/scripts/test.ts
@@ -1,6 +1,6 @@
 import { Application } from 'pixi.js'
 import { EntityIds } from '../../test/entityIds'
-import { InMemoryEventBus } from '../../infra/event/InMemoryEventBus'
+import { InMemoryEventBus } from '../../infra/eventBus/InMemoryEventBus'
 import { ConsoleLogger } from '../../Log/infra/consoleLogger'
 import { PixijsControllerAdapter } from '../../Systems/Controller/infra/PixijsControllerAdapter'
 import { PixijsDrawingAdapter } from '../../Systems/Drawing/infra/PixijsDrawingAdapter'
